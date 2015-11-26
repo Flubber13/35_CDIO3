2b449411dbbecaec2ed56ea0b6401442828ed048
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-11-26 17:11:33 +0100

    Fixed fleet rent

--- 35_CDIO3/src/fields/Fleet.java
+++ 35_CDIO3/src/fields/Fleet.java
@@ -48,10 +48,12 @@ public class Fleet extends AbstractOwnables{
 				GUI.showMessage(owner.getName()+ Text.landedOn + Text.fieldName[place]+ ". " + Text.youOwn);
 			}
 			else {
-				int newRent =(int)(rent*Math.pow(2, player.getNumFleetOwned())); // The rent the player depending on number of fleet owned
+				int newRent =(int)(rent*Math.pow(2, owner.getNumFleetOwned()-1)); // The rent the player depending on number of fleet owned
+				
 				GUI.showMessage(Text.owned + owner.getName() + ", " +  player.getName() + Text.mustPay + newRent + Text.to + owner.getName());
-				player.getAccount().addBalance(-rent*Math.pow(2, player.getNumFleetOwned()));
-				owner.getAccount().addBalance(rent*Math.pow(2, player.getNumFleetOwned()));
+				
+				player.getAccount().addBalance(-newRent);
+				owner.getAccount().addBalance(newRent);
 			}
 		}
 	}
