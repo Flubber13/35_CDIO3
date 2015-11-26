b3675a4a999464b0ff5ae96ace5c14050f44d247
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-11-26 16:36:39 +0100

    fjernede død kode setNumFleetOwned (vi bruger kun addNumFleetOnwned)

--- 35_CDIO3/src/game/Player.java
+++ 35_CDIO3/src/game/Player.java
@@ -38,10 +38,6 @@ public class Player {
 	public int getNumFleetOwned() {
 		return numFleetOwned;
 	}
-	
-	public void setNumFleetOwned(int numFleetOwned) {
-		this.numFleetOwned = numFleetOwned;
-	}
 
 	public void addNumFleetOwned(int numFleetOwned) {
 		this.numFleetOwned += numFleetOwned;
